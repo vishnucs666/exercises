7e4c3662177701323045841e80e213ebf58b5d13
vishnu <vishnucs477@gmail.com>
2018-07-11 15:17:33 +0530

    saved

--- roman.rb
+++ roman.rb
@@ -1,18 +1,19 @@
-
+# program to convert numbers to roman letters
 class ROMAN
- def self.romanize n
- 
-h = {"M"=> 1000,"D"=> 500,"C"=> 100,"L"=> 50,"X"=> 10,"V"=> 5, "I"=> 1}
-  roman = ""
+  def self.romanize(nnn)
+    # rubocop:disable LineLength
+    h = { 'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10, 'V' => 5, 'I' => 1 }
+    # rubocop:enable LineLength
+    roman = ''
 
-  h.each do |pair|
-    letter = pair[0]
-    value = pair[1]
-    roman += letter*(n / value)
-    n = n % value
+    h.each do |pair|
+      letter = pair[0]
+      value = pair[1]
+      roman += letter * (nnn / value)
+      nnn = nnn % value
+    end
+    puts roman
   end
-  puts roman
 end
-end
-k=gets.to_i
+k = gets.to_i
 ROMAN.romanize(k)
