433d9d47a9730a8f9f5b2c2a191ea9f30aa8a7fd
vishnu <vishnucs477@gmail.com>
2018-07-11 16:11:01 +0530

    gigabyte program modified

--- gig.rb
+++ gig.rb
@@ -1,12 +1,11 @@
+# program to find gigasecond
 class Gigasecond
-    def fr(time)
-      #time=10
-      time =time.to_i+1000000000
-      puts "time=#{time}"
-    end
+  def fr(time)
+    # time=10
+    time = time.to_i + 1_000_000_000
+    puts "time=#{time}"
+  end
 end
-s=gets.chomp
-r=Gigasecond.new
+s = gets.chomp
+r = Gigasecond.new
 r.fr(s)
-
-
